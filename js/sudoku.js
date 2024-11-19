ba4b8af27e6dfa21e0c9aa74ffca790b50721b42
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2024-11-19 16:51:30 +0000

    Start code on selecting square and inputting a symbol

--- js/sudoku.js
+++ js/sudoku.js
@@ -6,6 +6,8 @@ const size_each = size_square + size_border;  // Size of white square and one si
 
 var symbols_grid;   // a grid to store symbols
 var symbols_list = []; // List of symbols to display
+var cxt;    // the canvas context to draw
+var selectedSquare = null;    // coords on the selected square
 
 function draw() {
   // grid variable would be structured as this:
@@ -22,14 +24,16 @@ function draw() {
     }
   }
 
+  symbols_list = [];
+
   // Add in numbers
   if (size == 6)    // 6 is 36, need all 1-9, A-Z and 0 to be able to have enough of it
-    symbols_list.push(0);
+    symbols_list.push("0");
 
   if (size != 5) {  // if size is 5, just go with alphabet for A-Y
       const numbers_max = size_row < 9 ? size_row : 9;
       for (var i = 0; i < numbers_max; i++)
-        symbols_list.push(i + 1);
+        symbols_list.push((i + 1).toString());
   }
 
   // Add in alphabets
@@ -38,9 +42,14 @@ function draw() {
   for (var i = 0; i < alphabet_max; i++)
     symbols_list.push(alphabet[i]);
 
+  if (!rollSymbols()) {
+    // if a problem happens where one square can't accept any number, start all over again
+    draw();
+    return;
+  }
 
   const canvas = document.getElementById("sudoku");
-  const ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d");
 
   var size_whole = size_row * size_each + size_border;
 
@@ -60,39 +69,109 @@ function draw() {
 
       for (var gx = 0; gx < size; gx++) {
         for (var gy = 0; gy < size; gy++) {
-          var x = sx * size + gx;
-          var y = sy * size + gy;
-
-          var symbol = symbols_grid[sx][sy][gx][gy];
-          if (!symbol) {
-              // Select a random symbol to use, if not already set from checkLoneValidSymbols
-              var symbols_possible = getPossibleSymbols(sx, sy, gx, gy);
-              symbol = getRandomItemInArray(symbols_possible);
-              setSymbol(sx, sy, gx, gy, symbol);
-          }
-
-          // White squares
-          ctx.fillStyle = "white";
-          ctx.fillRect(x * size_each + size_border, y * size_each + size_border, size_square, size_square);
-
-          // Text
-          ctx.fillStyle = "black";
-          ctx.font = size_square + "px sans-serif";
-          ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-          ctx.fillText(symbol, (x + 0.5) * size_each + (size_border * 0.5), (y + 0.5) * size_each + size_border);
+          // Draw a white square
+          drawSquare(sx, sy, gx, gy, "white");
+          //drawText(sx, sy, gx, gy);
         }
       }
     }
   }
+
+  canvas.addEventListener('click', function(e) {
+    // Clear the selected square back to white
+    if (selectedSquare) {
+        drawSquare(selectedSquare.sx, selectedSquare.sy, selectedSquare.gx, selectedSquare.gy, "white");
+        selectedSquare = null;
+    }
+
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    x = Math.floor(x / size_each);
+    y = Math.floor(y / size_each);
+
+    var sx = Math.floor(x / size);
+    var sy = Math.floor(y / size);
+
+    // check if its out of bounds, if its the case then just ignore
+    if (sx < 0 || sx >= size || sy < 0 || sy >= size)
+        return;
+
+    // Set selectedSquare variable so it can be set back to white later when done
+    selectedSquare = {
+        sx: sx,
+        sy: sy,
+        gx: x - (sx * size),
+        gy: y - (sy * size),
+    }
+
+    drawSquare(selectedSquare.sx, selectedSquare.sy, selectedSquare.gx, selectedSquare.gy, "gold");
+    //drawText(sx, sy, gx, gy);
+  }, false);
+
+  document.addEventListener("keypress", onKeyPressed);
 }
 
-function setSymbol(sx, sy, gx, gy, symbol) {
-  symbols_grid[sx][sy][gx][gy] = symbol;
+function drawSquare(sx, sy, gx, gy, color) {
+    var x = sx * size + gx;
+    var y = sy * size + gy;
 
-  // Since we set one, search though for any lone symbol
-  checkLoneValidSymbols();
-  checkSymbolsInLoneSpot(sx, sy, gx, gy);
+    // draw a square at specific cords
+    ctx.fillStyle = color;
+    ctx.fillRect(x * size_each + size_border, y * size_each + size_border, size_square, size_square);
+}
+
+function drawText(sx, sy, gx, gy) {
+    var symbol = symbols_grid[sx][sy][gx][gy];
+    var x = (sx * size) + gx;
+    var y = (sy * size) + gy;
+
+    // Text
+    ctx.fillStyle = "black";
+    ctx.font = size_square + "px sans-serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(symbol, (x + 0.5) * size_each + (size_border * 0.5), (y + 0.5) * size_each + size_border);
+}
+
+function onKeyPressed(event) {
+    if (selectedSquare == null)
+        return; // we dont have a selected square to try this, ignore
+
+    var key = event.key;
+
+    if (symbols_list.indexOf(key) == -1)    // If the key pressed isnt in any of the possible symbols, ignore
+        return;
+
+    symbols_grid[selectedSquare.sx][selectedSquare.sy][selectedSquare.gx][selectedSquare.gy] = key;
+
+    // Clear the square back to white before drawing the text
+    drawSquare(selectedSquare.sx, selectedSquare.sy, selectedSquare.gx, selectedSquare.gy, "white");
+    drawText(selectedSquare.sx, selectedSquare.sy, selectedSquare.gx, selectedSquare.gy);
+    selectedSquare = null;
+}
+
+function rollSymbols() {
+    // This function is to roll what symbols it should be for each squares
+
+  for (var sx = 0; sx < size; sx++) {
+    for (var sy = 0; sy < size; sy++) {
+      for (var gx = 0; gx < size; gx++) {
+        for (var gy = 0; gy < size; gy++) {
+            if (symbols_grid[sx][sy][gx][gy])
+                continue;   // already set
+
+            var symbols_possible = getPossibleSymbols(sx, sy, gx, gy);
+            var symbol = getRandomItemInArray(symbols_possible);
+            symbols_grid[sx][sy][gx][gy] = symbol;
+            if (!checkLoneValidSymbols())
+                return false;
+        }
+      }
+    }
+  }
+
+  return true;
 }
 
 function checkLoneValidSymbols() {
@@ -106,60 +185,20 @@ function checkLoneValidSymbols() {
 
             var symbols_possible = getPossibleSymbols(sx, sy, gx, gy);
             if (symbols_possible.length == 1) {
-                setSymbol(sx, sy, gx, gy, symbols_possible[0]);
-                return;
+                symbols_grid[sx][sy][gx][gy] = symbols_possible[0];
+
+                // Since we just added one, start again on searching for another lone symbol
+                return checkLoneValidSymbols();
             } else if (symbols_possible.length == 0) {
-                throw new Error("No available symbol to select at [" + sx + "][" + sy + "][" + gx + "][" + gy + "]");
+                //throw new Error("No available symbol to select at [" + sx + "][" + sy + "][" + gx + "][" + gy + "]");
+                return false;
             }
         }
       }
     }
   }
-}
 
-function checkSymbolsInLoneSpot(sx1, sy1, gx1, gy1) {
-    // Check if symbol is available in only one specific section/row/col
-
-    // Used to store any lone spots found, "null" is set if its already set or there multiple spots available
-    var loneSpots = {};
-
-    // Check for whole section
-    for (var gx2 = 0; gx2 < size; gx2++) {
-        for (var gy2 = 0; gy2 < size; gy2++) {
-            var symbol = symbols_grid[sx1][sy1][gx2][gy2];
-            if (symbol) {
-                // already set
-                loneSpots[symbol] = null;
-                continue;
-            }
-
-            var symbols_possible = getPossibleSymbols(sx1, sy1, gx2, gy2);
-            for (var i = 0; i < symbols_possible.length; i++) {
-                symbol = symbols_possible[i];
-                if (symbol in loneSpots) {
-                    loneSpots[symbol] = null;   // This symbol is available in multiple spots
-                } else {
-                    // This symbol is currently only available in this
-                    loneSpots[symbol] = new Object();
-                    loneSpots[symbol].gx = gx2;
-                    loneSpots[symbol].gy = gy2;
-                }
-            }
-        }
-    }
-
-    console.log(loneSpots);
-
-    // After looking though all, check for any lone spots available for each symbol
-    for (var i = 0; i < symbols_list.length; i++) {
-        var symbol = symbols_list[i];
-        if (loneSpots[symbol] !== null) {
-            console.log(sx1, sy1, symbol);
-            console.log(loneSpots[symbol].gx, loneSpots[symbol].gy);
-            setSymbol(sx1, sy1, loneSpots[symbol].gx, loneSpots[symbol].gy, symbol);
-            return;
-        }
-    }
+  return true;
 }
 
 function getPossibleSymbols(sx1, sy1, gx1, gy1) {
